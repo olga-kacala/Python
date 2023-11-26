3ed6565b233e0fc7862caa7263b4992f74d007aa
olga-kacala <olga.kacala@gmail.com>
2023-11-26 17:10:45 +0100

    basic

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hello")
+
+x='1'
+y=2
+z=10.5
+
+x=float(x)
+y=float(y)
+suma=x+y+z
+
+a='eleven: '
+b= 11
+
+one = uno = jeden = '1'
+
+print(x)
+print(y)
+print(z)
+print(str(suma))
+print(a + str(b))
+print(one, uno, jeden)
