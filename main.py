078fe3ed349f2f9c0b6230c5763b790bf0f3dbad
olga-kacala <olga.kacala@gmail.com>
2023-12-04 12:07:46 +0100

    If statement python

--- main.py
+++ main.py
@@ -1,20 +1,79 @@
 
-x='1'
-y=2
-z=10.5
+# x='1'
+# y=2
+# z=10.5
 
-x=float(x)
-y=float(y)
-suma=x+y+z
+# x=float(x)
+# y=float(y)
+# suma=x+y+z
 
-a='eleven: '
-b= 11
+# a='eleven: '
+# b= 11
 
-one = uno = jeden = '1'
+# one = uno = jeden = '1'
 
-print(x)
-print(y)
-print(z)
-print(str(suma))
-print(a + str(b))
-print(one, uno, jeden)
+# print(x)
+# print(y)
+# print(z)
+# print(str(suma))
+# print(a + str(b))
+# print(one, uno, jeden)
+
+# x='1'
+# y=2
+# z=10.5
+
+# x=float(x)
+# y=float(y)
+# suma=x+y+z
+
+# a='eleven: '
+# b= 11
+
+# one = uno = jeden = '1'
+
+# print(x)
+# print(y)
+# print(z)
+# print(str(suma))
+# print(a + str(b))
+# print(one, uno, jeden)
+
+
+# web = 'http://google.com'
+# web2 = 'http://wikipedia.com'
+
+# slice = slice(7,-4)
+
+# print(web[slice])
+# print(web2[slice])
+
+# print(web[:7])
+
+# age = int(input('How old are you?'))
+
+# if age == 100: 
+#     print("You are 100")
+# elif age >=18:
+#     print("You are adult")
+# elif age <0:
+#     print("You are minus ")
+# else:
+#     print("You are baby")
+
+# rows=int(input('how many rows?'))
+# columns=int(input('how many columns?'))
+# symbol=input('symbol:')
+
+# for i in range(rows):
+#     for j in range(columns):
+#         print(symbol, end="")
+#     print()
+
+temp=int(input("What is the temp today?"))  
+
+if temp >=0 and temp <=30:
+    print("Go outside!")
+elif temp <0:
+    print("It is too cold")
+else: print("It is too hot")
